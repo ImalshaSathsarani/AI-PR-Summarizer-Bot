ec77a9d8c02c9356a75f883d3f286d3eb7bdbea7
Imalsha Sathsarani <158012123+ImalshaSathsarani@users.noreply.github.com>
2026-05-03 23:27:00 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from 'express'
+import { Octokit } from 'octokit'
+import { GoogleGenAI } from '@google/genai'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express();
+app.use(express.json());
+
+const genAI = new GoogleGenAI(process.env.GEMINI_API_KEY);
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+app.post('/webhook', async (req, res) => {
+    const { action, pull_request, repository } = req.body;
+
+    if(action == 'opened' || action == 'synchronize'){
+        const owner = repository.owner.login;
+        const repo = repository.name;
+        const pullNumber = pull_request.number;
+
+        try{
+            console.log(`Processing PR #${pullNumber} ... `);
+
+            const { data: diff } = await octokit.rest.pulls.get({
+                owner,
+                repo,
+                pull_number: pullNumber,
+                mediaType: {format:'diff'}
+            });
+
+            const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+            const prompt = `Summarize the following code changes into 3 clear bullet points for a human reviewer: \n\n ${diff} `;
+
+            const result = await model.generateContent(prompt);
+            const summary = result.response.text();
+
+            await octokit.rest.issues.createComment({
+                owner,
+                repo,
+                issue_number: pullNumber,
+                body: `### AI Code Summary\n\n ${summary}`,
+            });
+
+            console.log('Summary posted successfully!')
+
+        }catch(e){
+            console.log('Error processing PR', e)
+        }
+    }
+    res.status(200).send('OK');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Bot is listening on port ${process.env.PORT}`);
+})
